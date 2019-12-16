9be98a71ad70e676b3575b94f73c2faab87ccb9a
86159 <19930408>
2019-12-16 15:51:44 +0800

    one5

--- solo/src/main/java/com/pingan/solo/controller/TestController.java
+++ solo/src/main/java/com/pingan/solo/controller/TestController.java
@@ -5,4 +5,12 @@ public class TestController {
     public void show1(){
         System.out.println("a1");
     }
+
+    public void show2(){
+        System.out.println("a2");
+    }
+
+    public void show3(){
+        System.out.println("a3");
+    }
 }
